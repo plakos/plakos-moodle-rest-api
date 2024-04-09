e0afa4c66f0fe6acfa3ff2c2e33c3fa54b39f9f6
Benjamin Ansbach <137883114+uberneu@users.noreply.github.com>
2024-04-09 14:02:37 +0200

    Update version.php
    Updated version to allow installation for lernlink

--- version.php
+++ version.php
@@ -29,6 +29,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_ws_plakos';
 $plugin->version = 2023100900; // 4.3
 $plugin->requires = 2023100900;
-$plugin->supported = [403, 403];
+$plugin->supported = [400, 403];
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = 'v0.1.1';
